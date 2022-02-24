b699e0c6f6736bde79d6ef37f071670e662af3eb
Damola Awesu <62110318+DamolaAwesu@users.noreply.github.com>
2022-02-24 19:18:41 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "queue.h"
 
-long fibonacci(int n);
 extern volatile int count;
+
 int main(){
     pushQueue(5);
     pushQueue(10);
@@ -23,31 +23,8 @@ int main(){
     printf("Count: %d\n", count);
     for(int i = 0; i < count;i++)
         printf("Element %d: %d\n",i,queue[i]);
-    //fibonacci(30);
     printf("Tail = %d\n",tail);
     printf("Head = %d\n",head);
 
-    return 0;
-}
-
-long fibonacci(int n)
-{
-    int prev_result[] = {0,1};
-    int result = 0;int i = 2;
-    printf("%d " ,prev_result[0]);
-    printf("%d " ,prev_result[1]);
-    if (n < 2)
-        printf("%d\n " ,prev_result[0]+prev_result[1]);
-    else
-    {
-        while (i < n)
-        {
-            result = prev_result[0]+prev_result[1];
-            printf("%d ",result);
-            prev_result[0] = prev_result[1];
-            prev_result[1] = result;
-            i++;
-        }
-    }
     return 0;
 }
