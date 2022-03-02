60be97f0b77a62abe6e9737da7c6ca6e7e8ab9e0
Damola Awesu <62110318+DamolaAwesu@users.noreply.github.com>
2022-03-02 15:34:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main(){
     Std_ReturnType retVal = E_OK;
     int buf = 0;
-    #ifdef USE_HEAP
+    
     Queue_T* q = createQueue(10);
     Queue_T* q2 = createQueue(5);
     for(int i = 0; i < q->queueSize; i++)
@@ -18,22 +18,6 @@ int main(){
         printf("Val popped = %d\n",buf);
     }
     printf("Current count = %d\n",q->qcount);
-    #else
-    Queue_T q = createQueue(10);
-    Queue_T q2 = createQueue(5);
-    for(int i = 0; i < q.queueSize; i++)
-    {
-        retVal = q.queueInsert(&q,i*20);
-        printf("Val in queue = %d\n",q.queuePtr[i]);
-    }
-    for(int i = 0; i < 5; i++)
-    {
-        retVal = q.queuePop(&q,&buf);
-        printf("Val popped = %d\n",buf);
-    }
-    printf("Current count = %d\n",q.qcount);
-
-    #endif
     return 0;
 }
 
