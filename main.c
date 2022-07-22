8310ba8842e766dc40bc3da2cacd509269f88279
Damola Awesu <62110318+DamolaAwesu@users.noreply.github.com>
2022-07-22 23:15:20 +0200

    add queueConfig.h

--- main.c
+++ main.c
@@ -5,38 +5,38 @@ int main(){
     Std_ReturnType retVal = E_OK;
     int buf = 0;
     
-    Queue_T q = createQueue();
-    Queue_T q2 = createQueue();
-    for(int i = 0; i < q.queueSize; i++)
+    Queue_T* q = createQueue(10);
+    Queue_T* q2 = createQueue(10);
+    for(int i = 0; i < q->queueSize; i++)
     {
-        retVal = q.queueInsert(&q,i*20);
-        printf("Val in queue = %d\n",q.queuePtr[i]);
+        retVal = q->queueInsert(q,i*20);
+        printf("Val in queue = %d\n",q->queuePtr[i]);
     }
     for(int i = 0; i < 5; i++)
     {
-        retVal = q.queuePop(&q,&buf);
+        retVal = q->queuePop(q,&buf);
         printf("Val popped = %d\n",buf);
     }
-    printf("Current count = %d\n",q.qcount);
-    for (int i = 0; i < q.qcount; i++)
+    printf("Current count = %d\n",q->qcount);
+    for (int i = 0; i < q->qcount; i++)
     {
-        printf("Val in queue = %d\n",q.queuePtr[i]);
+        printf("Val in queue = %d\n",q->queuePtr[i]);
     }
     printf("\n");
-    for(int i = 0; i < q2.queueSize; i++)
+    for(int i = 0; i < q2->queueSize; i++)
     {
-        retVal = q2.queueInsert(&q2,i*20);
-        printf("Val inserted in queue 2 = %d\n",q2.queuePtr[i]);
+        retVal = q2->queueInsert(q2,i*20);
+        printf("Val inserted in queue 2 = %d\n",q2->queuePtr[i]);
     }
     for(int i = 0; i < 5; i++)
     {
-        retVal = q2.queuePop(&q2,&buf);
+        retVal = q2->queuePop(q2,&buf);
         printf("Val popped from queue 2= %d\n",buf);
     }
-    printf("Current count = %d\n",q2.qcount);
-    for (int i = 0; i < q2.qcount; i++)
+    printf("Current count = %d\n",q2->qcount);
+    for (int i = 0; i < q2->qcount; i++)
     {
-        printf("Val in queue 2 = %d\n",q2.queuePtr[i]);
+        printf("Val in queue 2 = %d\n",q2->queuePtr[i]);
     }
     
     return 0;
---
+++ queueConfig.h
@@ -0,0 +1,10 @@
+#ifndef QUEUE_CONFIG_H_
+#define QUEUE_CONFIG_H_
+
+#define USE_HEAP
+
+#ifndef USE_HEAP
+#define MAX_SIZE    (10)
+#endif
+
+#endif
